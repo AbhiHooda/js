d61ee43c213d422d3dbda5d7198fc046f58b5c5b
Abhishek Hooda <hooda.abhishek16@gmail.com>
2025-08-24 13:42:42 +0000

    while, do while

---
+++ iterators/while.js
@@ -0,0 +1,14 @@
+let condition = 0
+while (condition < 10) {
+    console.log(condition);    
+    condition  += 1
+}
+
+// it also support break and continue
+
+// do while executed atleast once
+condition = 40
+do {
+    console.log(condition);    
+    condition  -= 1    
+} while (condition > 20);
